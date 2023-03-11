812d9faba2aeb9e1ae90ff3d0db36b3d39d6b6b5
tolgaand <tolgaand@yandex.com>
2023-03-11 21:33:03 +0300

    refactor: edit in to return 1 or 0

--- programs/rivalz-goalflip/src/randomness_tools.rs
+++ programs/rivalz-goalflip/src/randomness_tools.rs
@@ -2,35 +2,22 @@ use anchor_lang::solana_program::keccak;
 use std::convert::TryInto;
 
 //https://docs.chain.link/docs/chainlink-vrf-best-practices/#getting-multiple-random-number
-pub fn expand(randomness: [u8; 32], n: u32) -> u32 {
+pub fn expand(randomness: [u8; 32]) -> u32 {
     let mut hasher = keccak::Hasher::default();
     hasher.hash(&randomness);
-    hasher.hash(&n.to_le_bytes());
 
-    u32::from_le_bytes(
-        hasher.result().to_bytes()[0..4]
-            .try_into()
-            .expect("slice with incorrect length"),
-    )
-}
+    let mut result = [0u8; 32];
+    hasher
+        .result()
+        .to_bytes()
+        .iter()
+        .enumerate()
+        .for_each(|(i, byte)| {
+            result[i % 4] ^= byte;
+        });
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn test_bit_slicing() {
-        let mut randomness = [0; 32];
-        randomness[0] = 1;
-        randomness[2] = 2;
-
-        let first_result = expand(randomness, 0);
-        println!("data: {:?}", randomness);
-        println!("{}", first_result);
-
-        randomness[0] = 255;
-        randomness[1] = 254;
-
-        let second_result = expand(randomness, 1);
-        assert_ne!(first_result, second_result);
-    }
+    let slice: [u8; 4] = result[0..4]
+        .try_into()
+        .expect("slice with incorrect length");
+    u32::from_le_bytes(slice) % 2 // Returns 0 or 1
 }
